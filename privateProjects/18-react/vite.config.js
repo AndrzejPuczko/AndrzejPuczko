a0d742298f91ee7d73a47b9bae99398a17ee19f7
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-25 19:08:04 +0200

    fix: add new feature for css modules settings. Hash classes on production

--- privateProjects/18-react/vite.config.js
+++ privateProjects/18-react/vite.config.js
@@ -1,16 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-	plugins: [react()],
-	base: '',
-	build: {
-		outDir: './dist',
-	},
-	css: {
-		modules: {
-			localsConvention: 'camelCase',
-			generateScopedName: '[name]_[local]__[hash:base64:5]',
+// export default defineConfig({
+// 	plugins: [react()],
+// 	base: '',
+// 	build: {
+// 		outDir: './dist',
+// 	},
+// 	css: {
+// 		modules: {
+// 			localsConvention: 'camelCase',
+// 			generateScopedName: '[name]_[local]__[hash:base64:5]',
+// 		},
+// 	},
+// })
+
+export default args => {
+	const generateScopedName = args.mode === 'production' ? '[hash:base64:5]' : '[name]_[local]__[hash:base64:5]'
+
+	return defineConfig({
+		plugins: [react()],
+		base: '',
+		build: {
+			outDir: './dist',
 		},
-	},
-})
+		css: {
+			modules: {
+				localsConvention: 'camelCase',
+				generateScopedName,
+			},
+		},
+	})
+}
