5610dd7d49cca55152f937915383d0e45d44a5a8
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-26 23:23:53 +0200

    feat: state lifting of generate user list

--- privateProjects/18-react/src/App.jsx
+++ privateProjects/18-react/src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react'
+
 import './App.module.scss'
 import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList'
 
 const App = () => {
+	const [usersList, setUsersList] = useState([])
+
+	const addUserHandler = generatedUser => {
+		setUsersList(prevUsersList => {
+			const updatedUsers = [...prevUsersList]
+			updatedUsers.unshift(generatedUser)
+			return updatedUsers
+		})
+	}
 	return (
 		<div>
-			<AddUser />
+			<AddUser generate={addUserHandler} />
+			<UsersList users={usersList} />
 		</div>
 	)
 }
