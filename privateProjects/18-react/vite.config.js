555626665a6fa785d5c47d273981265c14a184fc
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-26 23:12:39 +0200

    refactor: remove comment

--- privateProjects/18-react/vite.config.js
+++ privateProjects/18-react/vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-// 	plugins: [react()],
-// 	base: '',
-// 	build: {
-// 		outDir: './dist',
-// 	},
-// 	css: {
-// 		modules: {
-// 			localsConvention: 'camelCase',
-// 			generateScopedName: '[name]_[local]__[hash:base64:5]',
-// 		},
-// 	},
-// })
-
 export default args => {
 	const generateScopedName = args.mode === 'production' ? '[hash:base64:5]' : '[name]_[local]__[hash:base64:5]'
 
