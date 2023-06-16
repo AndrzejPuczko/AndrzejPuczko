86f849b3c49bd055a3ccf2a851ddf9134ed71dee
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-16 21:21:44 +0200

    fix: global css and functions to  App component

--- privateProjects/20 - FoodOrderApp/src/App.jsx
+++ privateProjects/20 - FoodOrderApp/src/App.jsx
@@ -1,10 +1,27 @@
-function App() {
- 
-  return (
-    <>
-     <h2>Let's get started!</h2>
-    </>
-  )
+import { useState } from 'react'
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+import Cart from './components/Cart/Cart'
+
+const App = () => {
+	const [cartIsShown, setCartIsShown] = useState(false)
+
+	const showCartHandler = () => {
+		setCartIsShown(true)
+	}
+	const hideCartHandler = () => {
+		setCartIsShown(false)
+	}
+
+	return (
+		<>
+			{cartIsShown && <Cart />}
+			<Header onShowCart={showCartHandler()} />
+			<main>
+				<Meals />
+			</main>
+		</>
+	)
 }
 
 export default App
