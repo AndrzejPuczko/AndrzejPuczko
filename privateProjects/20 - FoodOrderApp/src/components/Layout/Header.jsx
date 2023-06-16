ead165c070b9e9c67826d0ab54b897ba3e2e1dfa
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-16 21:13:58 +0200

    feat: add header component

---
+++ privateProjects/20 - FoodOrderApp/src/components/Layout/Header.jsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.css'
+import mealsImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = props => {
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>OrderMeals</h1>
+				<HeaderCartButton onCLick={props.onShowCart} />
+			</header>
+			<div className={styles.mainImage}>
+				<img src={mealsImage} alt="A table full of delicious food" />
+			</div>
+		</>
+	)
+}
+
+export default Header
