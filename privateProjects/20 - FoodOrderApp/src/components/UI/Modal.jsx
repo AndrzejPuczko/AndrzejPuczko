23961f4e8f2f9b0bea528cdab16b5f835044a75a
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-16 21:17:56 +0200

    feat: add modal component

---
+++ privateProjects/20 - FoodOrderApp/src/components/UI/Modal.jsx
@@ -0,0 +1,26 @@
+import styles from './Modal.module.css'
+import ReactDOM from 'react-dom'
+import { useState } from 'react'
+
+const Backdrop = props => <div className={styles.backdrop} />
+
+const ModalOverlay = props => {
+	return (
+		<div className={styles.modal}>
+			<div className={styles.content}>{props.children}</div>
+		</div>
+	)
+}
+
+const portalElement = document.getElementById('overlays')
+
+const Modal = props => {
+	return (
+		<>
+			{ReactDOM.createPortal(<Backdrop />, portalElement)}
+			{ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+		</>
+	)
+}
+
+export default Modal
