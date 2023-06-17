b0c153bb214334e25d84c3dea8a8195040b1d2cc
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-17 21:06:39 +0200

    fix: add provider to app

--- privateProjects/20 - FoodOrderApp/src/App.jsx
+++ privateProjects/20 - FoodOrderApp/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
 
 const App = () => {
 	const [cartIsShown, setCartIsShown] = useState(false)
@@ -14,13 +15,13 @@ const App = () => {
 	}
 
 	return (
-		<>
-			{cartIsShown && <Cart />}
-			<Header onShowCart={showCartHandler()} />
+		<CartProvider>
+			{cartIsShown && <Cart onClose={hideCartHandler}/>}
+			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
 			</main>
-		</>
+		</CartProvider>
 	)
 }
 
