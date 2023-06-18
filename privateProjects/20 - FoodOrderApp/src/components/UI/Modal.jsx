37cdd24f4192a242ff623c3fdb0b0167c7a19736
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-18 21:37:04 +0200

    fix: add prop functions

--- privateProjects/20 - FoodOrderApp/src/components/UI/Modal.jsx
+++ privateProjects/20 - FoodOrderApp/src/components/UI/Modal.jsx
@@ -1,8 +1,8 @@
 import styles from './Modal.module.css'
 import ReactDOM from 'react-dom'
-import { useState } from 'react'
 
-const Backdrop = props => <div className={styles.backdrop} />
+
+const Backdrop = props => <div className={styles.backdrop} onClick={props.onClose}  />
 
 const ModalOverlay = props => {
 	return (
@@ -17,7 +17,7 @@ const portalElement = document.getElementById('overlays')
 const Modal = props => {
 	return (
 		<>
-			{ReactDOM.createPortal(<Backdrop />, portalElement)}
+			{ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
 			{ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
 		</>
 	)
