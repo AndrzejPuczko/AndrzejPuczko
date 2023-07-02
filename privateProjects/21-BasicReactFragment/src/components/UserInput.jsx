2423c43a741fa434d4e1e758c5ab76dd779b6cbc
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-07-02 20:13:31 +0200

    fix: add required attribiute to input

--- privateProjects/21-BasicReactFragment/src/components/UserInput.jsx
+++ privateProjects/21-BasicReactFragment/src/components/UserInput.jsx
@@ -15,21 +15,21 @@ const UserInput = ({ onCalculate, onReset }) => {
 			<div className={styles.inputGroup}>
 				<p>
 					<label htmlFor="current-savings">Current Savings ($)</label>
-					<input type="number" id="current-savings" />
+					<input type="number" id="current-savings" required />
 				</p>
 				<p>
 					<label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-					<input type="number" id="yearly-contribution" />
+					<input type="number" id="yearly-contribution" required />
 				</p>
 			</div>
 			<div className={styles.inputGroup}>
 				<p>
 					<label htmlFor="expected-return">Expected Interest (%, per year)</label>
-					<input type="number" id="expected-return" />
+					<input type="number" id="expected-return" required />
 				</p>
 				<p>
 					<label htmlFor="duration">Investment Duration (years)</label>
-					<input type="number" id="duration" />
+					<input type="number" id="duration" required />
 				</p>
 			</div>
 			<p className={styles.actions}>
