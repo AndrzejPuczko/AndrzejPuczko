afb2bb12971d035a8c8437002a9e5d81055f5e93
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-07-02 19:58:18 +0200

    fix: add main calculate function and state

--- privateProjects/21-BasicReactFragment/src/App.jsx
+++ privateProjects/21-BasicReactFragment/src/App.jsx
@@ -1,33 +1,47 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import UserInput from './components/UserInput'
 import ResultsTable from './components/ResultsTable'
 
 function App() {
-	const calculateHandler = userInput => {
-		const yearlyData = []
+	const [investTable, setInvestTable] = useState(false)
+	const [investmentData, setInvestmentData] = useState({})
 
-		let currentSavings = +userInput['current-savings']
-		const yearlyContribution = +userInput['yearly-contribution']
-		const expectedReturn = +userInput['expected-return'] / 100
-		const duration = +userInput['duration']
+	const calculateHandler = userInput => {
+		const yearlyData = [] 
+
+		let currentSavings = +userInput.target['current-savings'].value 
+		let investedCapital = currentSavings
+		const yearlyContribution = +userInput.target['yearly-contribution'].value 
+		const expectedReturn = +userInput.target['expected-return'].value / 100
+		const duration = +userInput.target['duration'].value
 
 		for (let i = 0; i < duration; i++) {
 			const yearlyInterest = currentSavings * expectedReturn
 			currentSavings += yearlyInterest + yearlyContribution
+			investedCapital += yearlyContribution
 			yearlyData.push({
 				year: i + 1,
-				yearlyInterest: yearlyInterest,
-				savingsEndOfYear: currentSavings,
+				yearlyInterest: yearlyInterest.toFixed(2),
+				savingsEndOfYear: currentSavings.toFixed(2),
 				yearlyContribution: yearlyContribution,
+				investedCapital: investedCapital,
 			})
 		}
+		setInvestTable(true)
+		setInvestmentData(yearlyData)
+	}
+
+	const resetHandler = () => {
+		setInvestTable(false)
+		setInvestmentData({})
 	}
 
 	return (
 		<div>
 			<Header />
-			<UserInput />
-			<ResultsTable />
+			<UserInput onCalculate={calculateHandler} onReset={resetHandler} />
+			{investTable ? <ResultsTable investmentData={investmentData} /> : <p className="errorMessage">Fill in all fields of the form</p>}
 		</div>
 	)
 }
