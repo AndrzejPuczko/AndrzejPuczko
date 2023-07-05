4989e854e9f202dd29cf39f1e415d45538253837
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-07-05 15:09:30 +0200

    fix: add states and dependencies for fetching

--- privateProjects/22-ReactAndAPI/src/App.jsx
+++ privateProjects/22-ReactAndAPI/src/App.jsx
@@ -5,19 +5,33 @@ import './App.css'
 
 function App() {
 	const [movies, setMovies] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
 
 	const fetchMoviesHandler = async () => {
-		const response = await fetch('https://swapi.dev/api/films/')
-		const data = await response.json()
-		const transformedMovies = data.results.map(movieData => {
-			return {
-				id: movieData.episode_id,
-				title: movieData.title,
-				openingText: movieData.opening_crawl,
-				releaseDate: movieData.release_date,
+		setIsLoading(true)
+		setError(null)
+
+		try {
+			const response = await fetch('https://swapi.dev/api/films/')
+			if (!response.ok) {
+				throw new Error('Something went wrong!')
 			}
-		})
-		setMovies(transformedMovies)
+
+			const data = await response.json()
+			const transformedMovies = data.results.map(movieData => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				}
+			})
+			setMovies(transformedMovies)
+		} catch (error) {
+			setError(error.message)
+		}
+		setIsLoading(false)
 	}
 
 	return (
@@ -26,7 +40,9 @@ function App() {
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
 			<section>
-				<MoviesList movies={movies} />
+				{isLoading && <p>Loading...</p>}
+				{!isLoading && <MoviesList movies={movies} />}
+				{!isLoading && error && <p>Something went wrong ...</p>}
 			</section>
 		</React.Fragment>
 	)
